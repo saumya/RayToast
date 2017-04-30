82c96410ed75cc55e273fff6f9fe9ed6bf280e2d
saumya <2saumyaray@gmail.com>
2017-04-30 17:32:21 +0530

    Code cleanup

--- RayToast.hx
+++ RayToast.hx
@@ -5,7 +5,7 @@ import lime.system.JNI;
 
 class RayToast {
 	
-	public static function testToast(msg:String):Void{
+	public static function testToast():Void{
 		testToastJNI();
 	}
 	private static var testToastJNI = JNI.createStaticMethod("org.haxe.extension.RayToast", "testToast", "()V");
